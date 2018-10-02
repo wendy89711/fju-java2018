904bc432e2297d9689630e26e05fd093f8a09f52
MICROSOFT <MICROSOFT@DESKTOP-C4BOVOA>
2018-10-02 17:19:29 +0800

    exam

--- exam/src/com/fju/Hello/Hello.java
+++ exam/src/com/fju/Hello/Hello.java
@@ -4,7 +4,19 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int age = 18;
+        float weight = 44.4f;
+        float height = 151.3f;
+        String name = "Wendy";
+        int schoolYear = 107;
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(height);
+        System.out.println(name);
+        System.out.println(schoolYear);
+        
+        
+        
 	}
 
 }
