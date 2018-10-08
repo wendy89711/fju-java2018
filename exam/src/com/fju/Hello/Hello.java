cae1329bebac3482959fc5edef96cbf37d812a5e
MICROSOFT <MICROSOFT@DESKTOP-C4BOVOA>
2018-10-08 15:17:08 +0800

    text

--- exam/src/com/fju/Hello/Hello.java
+++ exam/src/com/fju/Hello/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int age = 18;
+		/*int age = 18;
         float weight = 44.4f;
         float height = 1.513f;
         String name = "Wendy";
@@ -17,7 +17,7 @@ public class Hello {
         float bmi = weight/ (height * height);
         System.out.println(bmi);
         age = age +1;
-        System.out.println(age);
+        System.out.println(age);*/
         
    
         
