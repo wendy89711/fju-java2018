fe991a4dd8d85ad67d696258fea6b89ab5d7641f
Wendy <user@es302n25>
2018-10-09 12:01:49 +0800

    10/09

--- myproject/src/com/wen/Hello.java
+++ myproject/src/com/wen/Hello.java
@@ -4,7 +4,17 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello.world");
+		Person p = new Person(44.4f , 1.513f);
+//		p.weight = 44.4f;
+//		p.height = 1.513f;
+		System.out.println(p.bmi());
+		p.hello();
+		Person hank = new Person(55.4f , 1.8f);
+		System.out.println(hank.bmi());
+//      Person jack = null;
+//      jack.hello();
+		
+        /*System.out.println("Hello.world");
         int age = 18;
         float weight = 44.4f;
         float height = 151.3f;
@@ -13,7 +23,7 @@ public class Hello {
         System.out.println(age);
         System.out.println(weight);
         System.out.println(name);
-        System.out.println(schoolYear);
+        System.out.println(schoolYear);*/
         
         
 	}
